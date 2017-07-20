a45b1b7a975ec0f4d349f4f99e97d9abbd805be8
DESKTOP-JA6MH3M\burha <burhanshakir96@gmail.com>
2017-07-20 09:59:37 +0530

    Change in layouts

--- app/src/main/java/com/burhan/quicktooldialogbox/MainActivity.java
+++ app/src/main/java/com/burhan/quicktooldialogbox/MainActivity.java
@@ -2,13 +2,24 @@ package com.burhan.quicktooldialogbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import burhan.com.material_quicktooldialogbox.QuickToolDialog;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        QuickToolDialog quickToolDialog = new QuickToolDialog(this, QuickToolDialog.HORIZONTAL, QuickToolDialog.LIGHT_THEME);
+
+        quickToolDialog.setButtons(R.xml.quicktool_dialog_items);
     }
 }
